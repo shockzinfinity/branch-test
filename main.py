cb750650591f87c056235b0dee4400169c519b43
Jun Yu (RTX) <shockzinfinity@gmail.com>
2023-09-18 11:27:02 +0900

    feat: Functionalize and Edit message

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-for i in range(5):
-  print(f'hello for {i}th times')
+def print_hello(num: int):
+  """
+  Print hello
+  """
+  for i in range(num):
+    print(f'{i + 1}번째 안녕')
+    
+if __name__ == '__main__':
+  print_hello(10)
