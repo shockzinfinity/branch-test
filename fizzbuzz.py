e06dc79f6e13f21d0e2f62a35074556de445c995
Jun Yu (RTX) <shockzinfinity@gmail.com>
2023-09-18 12:06:56 +0900

    feat: Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def do_fizzbuzz(num: int):
+  """
+  fizzbuzz 기능을 수행합니다.
+  정해진 숫자에 대해,
+  3의 배수는 'fizz'
+  5의 배수는 'buzz'
+  15의 배수는 'fizzbuzz'
+  나머지는 숫자 그대로를 출력합니다.
+
+  Args:
+      num (int): 실행할 숫자 범위
+  """
+  print('hello')
